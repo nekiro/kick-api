b9df320926cfcd044bf1b645f4a8c066e5ed6f19
Marcin Jałocha <nekiro.dev@gmail.com>
2025-07-31 23:02:00 +0200

    fix q to be required in getCategories

--- src/modules/categories.ts
+++ src/modules/categories.ts
@@ -1,16 +1,19 @@
 import type { ApiResponse, Category, PaginatedResponse, RequestFn } from "../types";
+import { KickBadRequestError } from "../errors";
 
 export class CategoriesModule {
 	private readonly baseRoute = "/public/v1/categories";
 
 	constructor(private request: RequestFn) {}
 
-	async getCategories(params?: { q: string; page?: number }): Promise<PaginatedResponse<Category>> {
+	async getCategories(params: { q: string; page?: number }): Promise<PaginatedResponse<Category>> {
+		if (!params || !params.q) {
+			throw new KickBadRequestError("q is required");
+		}
+
 		const searchParams = new URLSearchParams();
 
-		if (params?.q) {
-			searchParams.append("q", params.q);
-		}
+		searchParams.append("q", params.q);
 
 		if (params?.page) {
 			searchParams.append("page", params.page.toString());
